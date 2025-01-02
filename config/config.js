78cca4a22c8afa9aace52d352cd8aafda969f081
Beto Villalba <betovill26@gmail.com>
2025-01-02 14:48:13 -0300

    config .env

---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.MYSQL_DATABASE || "db_resonancia",
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306,
+  },
+  test: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.MYSQL_DATABASE || "database_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306,
+  },
+  production: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.MYSQL_DATABASE || "database_production",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306,
+  },
+};
